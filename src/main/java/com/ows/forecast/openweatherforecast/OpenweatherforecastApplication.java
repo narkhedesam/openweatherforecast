f38434cdaee659a0d321f8f560774c7ec4b0f096
Sameer Narkhede <narkhedesam@gmail.com>
2022-06-23 09:33:18 +0000

    initial commit

---
+++ src/main/java/com/ows/forecast/openweatherforecast/OpenweatherforecastApplication.java
@@ -0,0 +1,13 @@
+package com.ows.forecast.openweatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenweatherforecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenweatherforecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ows/forecast/openweatherforecast/OpenweatherforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ows.forecast.openweatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenweatherforecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
